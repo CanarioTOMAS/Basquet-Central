c885ec446e871788a997002d777436f8bc2fd701
CanarioTOMAS <tomimartine.34@gmail.co>
2022-08-16 16:53:56 -0300

    work in progres

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React,{useState} from 'react';
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
-import ModalInfo from './components/ModalInfo/ModalInfo';
 import Canvas from './components/Canvas/Canvas';
-import { Button, Modal } from "react-bootstrap";
-import ModalPurchase from './components/ModalPurchase/ModalPurchase';
-import foto1 from "./image/cluc.jpeg";
 import foto2 from "./image/logo2.png";
 import ModalDescription from './components/ModalDescription/ModaDescription';
 
@@ -15,7 +13,7 @@ function App() {
 
   return (
   <div className="App">
-    <div className="container-fluid">
+    <div className=" container-fluid">
       <div className="row">
         <div className="col-3">
           <img className="logo-central" src={foto2}></img>
@@ -37,9 +35,9 @@ function App() {
       } } />
 <Canvas />
       <div className="fluid">
-    <div className="row separar">
-    <div className='col'></div>
-      <div className="col-3 contenedor1">
+    <div className="row separar" >
+    <div className='col-sm'></div>
+      <div className="col-sm-3  contenedor1" >
         <p className="contacto ">Contacto</p>
         <p className="cel">3571-524930 - Ivana</p>
         <p className="cel">3571-550548 - Federico</p>
@@ -47,23 +45,23 @@ function App() {
         <p className="cel">3571-565906 - Laura</p>
       </div>
 
-      <div className="col-3 contenedor2">
+      <div className="col-sm-3 contenedor2">
         <p className="contacto">Redes</p>
         <p className="cel"> Instagram =<a className='link' href='https://instagram.com/basquet.csca?igshid=YmMyMTA2M2Y=' > @Basquet.csca</a></p>
         <p className="cel"> Facebook = <a className='link' href='https://m.facebook.com/profile.php?id=100029905552517' >@Basquet.csca</a></p>
       </div>
       
-      <div className="col-3 contenedor3">
+      <div className="col-sm-3 contenedor3">
         <p className="contacto">Formas de Pago</p>
         <p className="cel">$5000 el m<sup>2</sup></p>
         <p className="cel">°Efectivo</p>
         <p className="cel">°Transferencia</p>
         <p className="cel">°3 cuotas sin interés</p>
       </div>
-      <div className='col'></div> 
+      <div className='col-sm'></div> 
    </div>
 </div>
-<div className='row separar'>
+<div className='row-sm separar'>
 <footer>
     Esta aplicacion fue creada por <a href="http://socialup.com.ar/"> SocialUp</a>
    </footer>
--- src/components/ModalDescription/ModaDescription.tsx
+++ src/components/ModalDescription/ModaDescription.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Modal } from "react-bootstrap";
 import {block} from '../../interfaces/block'
 import { soldOutBlock } from "../../interfaces/soldOutBlock";
+import './ModalDescription.css'
 
 interface ModalDescriptionProps{
     soldOutBlock?:soldOutBlock;
@@ -21,12 +22,12 @@ const ModalDescription: React.FC<ModalDescriptionProps> = ({show,soldOutBlock,ha
                 CAMPAÑA “UN METRO POR UN SUEÑO” – 500 m2.                
                </Modal.Title>
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body> <p className="parrafo">
             En Central queremos contarte que vamos a poner el piso deportivo.  Después de 67 años de Historias en el Club, nos embarcamos en el difícil objetivo de poner el piso de Parquet en el estadio “La Caldera”.  
 Su presidente Aldo Falco, junto a los vecinos Alberto Rosso, Angel Luraschi, José Randoni, Ernesto Romasotto, Tomás Rosso, Francisco Rosso y Oscar Villalón junto a tantos dirigentes que pasaron por Central seguramente soñaron con lograrlo.  Hoy lo podemos hacer realidad con tu participación.
 Te invitamos a ser parte de este SUEÑO adquiriendo un metro cuadrado simbólico para hacer realidad la instalación del piso que ya compramos y que está guardado en el Club.
 Podes hacerlo a través de la página web www.unmetroporunsueño.com.ar  o comunicándote con  la Subcomisión de Básquet al celular 3571 524930.
-
+</p>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
